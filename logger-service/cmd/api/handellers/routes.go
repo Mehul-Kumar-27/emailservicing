f57f9047307a9795923ce5b1d06059ab9dbae1c9
Mehul-Kumar-27 <mehul2002kumar@gmail.com>
2024-01-03 01:45:07 +0530

    adding logger sevice errors to be corrected

---
+++ logger-service/cmd/api/handellers/routes.go
@@ -0,0 +1,26 @@
+package handellers
+
+import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
+)
+
+func (app *LoggerService) Routes() *chi.Mux {
+	mux := chi.NewRouter()
+	mux.Use(middleware.Logger)
+
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		MaxAge:           300,
+		AllowCredentials: true,
+	}))
+
+	mux.Use(middleware.Heartbeat("/ping"))
+	mux.Post("/log", app.WriteLog)
+
+	return mux
+}
